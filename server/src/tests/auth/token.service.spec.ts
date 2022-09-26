83f9eb07fa9a75ccebd8e00e8295050b93ff7dc3
robert-brown96 <robert.brown@saascg.com>
2022-09-26 18:51:21 -0500

    seeding

--- server/src/tests/auth/token.service.spec.ts
+++ server/src/tests/auth/token.service.spec.ts
@@ -2,31 +2,43 @@
 //@ts-ignore
 import { TokenServiceBase } from "../../auth/base/token.service.base";
 import {
-  INVALID_USERNAME_ERROR,
-  INVALID_PASSWORD_ERROR,
-  //@ts-ignore
+    INVALID_USERNAME_ERROR,
+    INVALID_PASSWORD_ERROR
+    //@ts-ignore
 } from "../../auth/constants";
 
 describe("Testing the TokenServiceBase", () => {
-  let tokenServiceBase: TokenServiceBase;
-  beforeEach(() => {
-    tokenServiceBase = new TokenServiceBase();
-  });
-  describe("Testing the BasicTokenService.createToken()", () => {
-    it("should create valid token for given username and password", async () => {
-      expect(await tokenServiceBase.createToken("admin", "admin")).toBe(
-        "YWRtaW46YWRtaW4="
-      );
+    let tokenServiceBase: TokenServiceBase;
+    beforeEach(() => {
+        tokenServiceBase = new TokenServiceBase();
     });
-    it("should reject when username missing", () => {
-      //@ts-ignore
-      const result = tokenServiceBase.createToken(null, "admin");
-      return expect(result).rejects.toBe(INVALID_USERNAME_ERROR);
+    describe("Testing the BasicTokenService.createToken()", () => {
+        it("should create valid token for given username and password", async () => {
+            expect(
+                await tokenServiceBase.createToken({
+                    username: "admin",
+                    password: "admin",
+                    id: ""
+                })
+            ).toBe("YWRtaW46YWRtaW4=");
+        });
+        it("should reject when username missing", () => {
+            //@ts-ignore
+            const result = tokenServiceBase.createToken({
+                username: "",
+                password: "admin",
+                id: ""
+            });
+            return expect(result).rejects.toBe(INVALID_USERNAME_ERROR);
+        });
+        it("should reject when password missing", () => {
+            //@ts-ignore
+            const result = tokenServiceBase.createToken({
+                username: "admin",
+                password: "",
+                id: ""
+            });
+            return expect(result).rejects.toBe(INVALID_PASSWORD_ERROR);
+        });
     });
-    it("should reject when password missing", () => {
-      //@ts-ignore
-      const result = tokenServiceBase.createToken("admin", null);
-      return expect(result).rejects.toBe(INVALID_PASSWORD_ERROR);
-    });
-  });
 });
