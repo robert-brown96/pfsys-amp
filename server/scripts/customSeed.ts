01ed5a60e85f02a2cb0c5bc42fde97218d7a5fb5
robert-brown96 <robert.brown@saascg.com>
2022-09-25 20:45:53 -0500

    changes

--- server/scripts/customSeed.ts
+++ server/scripts/customSeed.ts
@@ -1,17 +1,83 @@
 import { PrismaClient } from "@prisma/client";
 
 export async function customSeed() {
-  const client = new PrismaClient();
-  const username = "admin";
+    const client = new PrismaClient();
+    const username = "admin";
 
-  //replace this sample code to populate your database
-  //with data that is required for your service to start
-  await client.user.update({
-    where: { username: username },
-    data: {
-      username,
-    },
-  });
+    //replace this sample code to populate your database
+    //with data that is required for your service to start
+    await client.user.update({
+        where: { username: username },
+        data: {
+            username
+        }
+    });
 
-  client.$disconnect();
+    await client.user.upsert({
+        where: { username: "system" },
+        update: {
+            username: "system",
+            email: "pfsystem.brown@gmail.com",
+            roles: ["admin"],
+            password: "Guest"
+        },
+        create: {
+            username: "system",
+            email: "pfsystem.brown@gmail.com",
+            roles: ["admin"],
+            password: "Guest"
+        }
+    });
+
+    await client.book.upsert({
+        where: { name: "Primary" },
+        update: {
+            name: "Primary",
+            isPrimary: true,
+            primaryCurrency: {
+                connectOrCreate: {
+                    where: { code: "USD" },
+                    create: { code: "USD", name: "US Dollar" }
+                }
+            }
+        },
+        create: {
+            name: "Primary",
+            isPrimary: true,
+            primaryCurrency: {
+                connectOrCreate: {
+                    where: { code: "USD" },
+                    create: { code: "USD", name: "US Dollar" }
+                }
+            },
+            owner: { connect: { username: "system" } }
+        }
+    });
+
+    await client.book.upsert({
+        where: { name: "Adjustment" },
+        update: {
+            name: "Adjustment",
+            isPrimary: false,
+            primaryCurrency: {
+                connectOrCreate: {
+                    where: { code: "USD" },
+                    create: { code: "USD", name: "US Dollar" }
+                }
+            }
+        },
+        create: {
+            name: "Adjustment",
+            primaryCurrency: {
+                connectOrCreate: {
+                    where: { code: "USD" },
+                    create: { code: "USD", name: "US Dollar" }
+                }
+            },
+            isPrimary: false,
+            owner: { connect: { username: "system" } }
+        }
+    });
+
+    client.$disconnect();
 }
